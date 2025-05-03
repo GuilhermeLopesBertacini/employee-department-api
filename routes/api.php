26470f6662c1972782a324c3d8b6574e0eb30af8
GuilhermeLopesBertacini <guilb2005@gmail.com>
2025-05-03 11:26:47 -0300

    added routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,25 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FuncionarioController;
+use App\Http\Controllers\DepartamentoController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// FUNCIONÁRIOS
+Route::get('/funcionarios', [FuncionarioController::class, 'index']);
+Route::get('/funcionarios/{id}', [FuncionarioController::class, 'show']);
+Route::post('/funcionarios', [FuncionarioController::class, 'store']);
+Route::put('/funcionarios/{id}', [FuncionarioController::class, 'update']);
+Route::delete('/funcionarios/{id}', [FuncionarioController::class, 'destroy']);
+
+// DEPARTAMENTOS
+Route::get('/departamentos', [DepartamentoController::class, 'index']);
+Route::get('/departamentos/{id}', [DepartamentoController::class, 'show']);
+Route::post('/departamentos', [DepartamentoController::class, 'store']);
+Route::put('/departamentos/{id}', [DepartamentoController::class, 'update']);
+Route::delete('/departamentos/{id}', [DepartamentoController::class, 'destroy']);
+
+// RELACIONAMENTOS
+Route::get('/funcionarios/{id}/departamento', [FuncionarioController::class, 'getDepartamento']);
+Route::get('/departamentos/{id}/funcionarios', [DepartamentoController::class, 'getFuncionarios']);
+Route::get('/relatorio/funcionarios-com-departamentos', [FuncionarioController::class, 'listarComDepartamentos']);
+Route::get('/relatorio/departamentos-com-funcionarios', [DepartamentoController::class, 'listarComFuncionarios']);
